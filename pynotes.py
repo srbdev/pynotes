792a9d83d134a754c70ded2d907bdd192348dfe7
sb <sylvain.rb@gmail.com>
2018-02-20 19:46:27 -0700

    Add list notes for a topic functionality

--- pynotes.py
+++ pynotes.py
@@ -11,6 +11,7 @@ EDITOR = os.environ.get("EDITOR", "vim")
 parser = argparse.ArgumentParser(description="Write and manage notes from the command line.")
 parser.add_argument("-c", "--create", help="create a new topic", metavar="topic")
 parser.add_argument("-a", "--add", help="add a note to a topic", metavar="topic")
+parser.add_argument("-l", "--list", help="list notes from a topic", metavar="topic")
 
 def run_editor():
     with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
@@ -21,7 +22,7 @@ def run_editor():
 def get_connection():
     return sqlite3.connect(DATABASE)
 
-def run_query(query, tuple=None):
+def runsql(query, tuple=None):
     conn = get_connection()
 
     if tuple is not None:
@@ -31,14 +32,34 @@ def run_query(query, tuple=None):
     conn.commit()
     conn.close()
 
+def fetchall(query, tuple=None):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    if tuple is not None:
+        cur.execute(query, tuple)
+    else:
+        cur.execute(query)
+
+    return cur.fetchall()
+
 def create_topic(name):
     sql = """CREATE TABLE %s(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, note TEXT, created_at TIMESTAMP, modified_at TIMESTAMP)""" % name.strip()
-    run_query(sql)
+    runsql(sql)
 
 def add_note(name):
     note = run_editor()
     timestamp = datetime.utcnow()
-    run_query("INSERT INTO %s (note, created_at, modified_at) VALUES (?, ?, ?)" % name, (note, timestamp, timestamp))
+    runsql("INSERT INTO %s (note, created_at, modified_at) VALUES (?, ?, ?)" % name, (note, timestamp, timestamp))
+
+def list_notes(topic):
+    sql = """SELECT * FROM %s""" % topic
+    rows = fetchall(sql)
+    str_length = 25
+
+    for row in rows:
+        print "(%s) %s - last modified: %s" % (row[0], (row[1][:str_length] + "...").encode('unicode_escape') if len(row[1]) > str_length+3 else row[1].encode('unicode_escape'), row[3])
+
 
 args = parser.parse_args()
 
@@ -46,5 +67,7 @@ if args.add is not None:
     add_note(args.add)
 elif args.create is not None:
     create_topic(args.create)
+elif args.list is not None:
+    list_notes(args.list)
 else:
     print parser.print_help()
